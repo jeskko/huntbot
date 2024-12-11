de41066a28aec71eb4d7945b7e297d1b3f24e8e6
Jarkko Vääräniemi (OH8HQL) <oh8hql@gmail.com>
2024-12-11 19:22:18 +0200

    fix the fix

--- nuny/misc_utils.py
+++ nuny/misc_utils.py
@@ -80,7 +80,7 @@ def process_despawn(status,time):
             start=ts
             end=8640-ts
             
-    if status=="Up" or status=="Scouting" or status == "Spawning" or status=="Despawning":
+    if status=="Up" or status=="Scouting" or status=="Despawning":
         for d in nuny.config.conf["despawn"]:
             if ts>=d["start"] and ts<d["end"]:
                 if d["status"]!="Up":
